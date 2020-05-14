7a3a42e2c193b2dd081fc648beffcbafa0bcea14
Ray Hidayat <ray.hidayat@gmail.com>
2020-05-14 19:03:22 +1200

    All neighbours can be incapacitating

--- src/agent/OccupantMap.ts
+++ src/agent/OccupantMap.ts
@@ -52,8 +52,7 @@ export class OccupantMap {
         }
         for (const enemy of beliefs.pacs.values()) {
             if (enemy.team === w.Teams.Enemy && enemy.alive && enemy.seenTick === beliefs.tick) {
-                const maxMovementSpeed = AgentHelper.maxMovementSpeed(enemy, beliefs);
-                for (const n of traverse.untilRange(enemy.pos, maxMovementSpeed, beliefs)) {
+                for (const n of traverse.neighbours(enemy.pos, beliefs)) {
                     if (selfLocations.has(n.hash()) && !occupants[n.y][n.x]) {
                         // Detected the enemy could move to the same square as us
                         occupants[n.y][n.x] = this.pacOccupant(enemy, beliefs);
